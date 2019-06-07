f0425d22725b87e588b7cdc186a9220897a4c6bf
Connor Turland <connorturland@gmail.com>
2019-06-07 15:47:15 -0400

    duh just use the prebuilt

--- script.sh
+++ script.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
 
-git clone https://github.com/holochain/n3h.git
-
-cd n3h
-npm install
-
-./build/release-build-electron.bash
-
-cd build/electron-build/dist
 export ELECTRON_ENABLE_LOGGING=true
 ./n3h-0.0.13-alpha-win-x64.exe --version
