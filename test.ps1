8b916ee1ab0e6a4f29b518c364c31cb41eda0474
Connor Turland <connorturland@gmail.com>
2019-06-07 20:42:46 -0400

    try powershell

--- test.ps1
+++ test.ps1
@@ -1,2 +1,2 @@
 "hello hello"
-& 'C:/Users/travis/build/Connoropolous/windows-ci-test/n3h-0.0.13-alpha-win-x64.exe --version'
+& 'C:/Users/travis/build/Connoropolous/windows-ci-test/n3h-0.0.13-alpha-win-x64.exe'
